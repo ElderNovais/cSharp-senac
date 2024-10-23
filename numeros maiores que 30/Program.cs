fd18631c4505bae604a68744f88721903ed67890
ElderNovais <novaisselder@gmail.com>
2024-10-23 14:55:59 -0300

    Add files via upload

---
+++ numeros maiores que 30/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_maiores_que_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0, num, i;
+
+            for (i = 1; i <= 30; i++)
+            {
+                Console.WriteLine("Digite um numero: ");
+                num = int.Parse(Console.ReadLine());
+                if (num>30)
+                {
+                    cont++;
+                }
+            }
+
+            Console.Write("qtd. de números maiores que 30: ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ quadrados de numeros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quadrados_de_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            for(int i = 1; i<=10; i++)
+            {
+                Console.Write("Digite um numero: ");
+                num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num + "² = " + Math.Pow(num, 2));
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
